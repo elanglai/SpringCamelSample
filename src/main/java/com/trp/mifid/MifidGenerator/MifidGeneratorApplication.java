c0ba7b174d4bab39257822dca47a9afaa2834c4c
ericlanglais <eric.langlais@gmail.com>
2017-11-07 20:37:01 -0500

    Code shell created using Spring Initializer tool. Generate a maven Project with Java and Spring Boot 1.5.8 along with the following starter dependencies: Lombok, Jersey, Freemarker, H2, Apache Camel, Actuator

---
+++ src/main/java/com/trp/mifid/MifidGenerator/MifidGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.trp.mifid.MifidGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MifidGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MifidGeneratorApplication.class, args);
+	}
+}
